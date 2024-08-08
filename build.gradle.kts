86b4461544adee6f1080d256d6e624985120afba
itstor <ahmdthoriq5@gmail.com>
2024-08-08 07:52:41 +0700

    chore(deps): upgrade to keycloak 25.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,16 @@ plugins {
 }
 
 group = "com.itstor.keycloak-conditional-clientid"
-version = "1.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.keycloak:keycloak-services:24.0.1")
-    implementation("org.keycloak:keycloak-server-spi:24.0.1")
-    implementation("org.keycloak:keycloak-server-spi-private:24.0.1")
+    implementation("org.keycloak:keycloak-services:25.0.2")
+    implementation("org.keycloak:keycloak-server-spi:25.0.2")
+    implementation("org.keycloak:keycloak-server-spi-private:25.0.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
