c42043c34cc27a3f938a8bf9f11b467ab6e6fb72
itstor <ahmdthoriq5@gmail.com>
2024-10-05 13:45:51 +0700

    chore(deps): upgrade to keycloak 26.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ repositories {
 }
 
 dependencies {
-    implementation("org.keycloak:keycloak-services:25.0.2")
-    implementation("org.keycloak:keycloak-server-spi:25.0.2")
-    implementation("org.keycloak:keycloak-server-spi-private:25.0.2")
+    implementation("org.keycloak:keycloak-services:26.0.0")
+    implementation("org.keycloak:keycloak-server-spi:26.0.0")
+    implementation("org.keycloak:keycloak-server-spi-private:26.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
